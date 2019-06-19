e9154c3321bbd3b2f672d72d85dc1b1a5e53baa1
Igor Ivanov <drkierkegor@gmail.com>
2019-06-19 13:48:35 +0300

    updt of validation process and +step 2

--- proto/main.js
+++ proto/main.js
@@ -1,14 +1,19 @@
 const errors = {
-    emptyFields: 'Please, fill each field',
+    emptyField: 'Please, fill the field',
     notANumber: 'Please, use only numbers',
+    usingSpaces: 'Please, don\'t use spaces',
+    tooBigAmount: 'The amount should be less or equal to 100',
 }
+
+// regExps
 
 const regExpObj = {
     regExpNum: /\d/g,
     regExpSpaces: /\s/g,
 }
 
-const errorBlock = $('.error-block');
+
+// hides blocks
 
 function hideBlock(block) {
     setTimeout(function() {
@@ -19,22 +24,74 @@ function hideBlock(block) {
 function hideErrorBlock(currentForm, block) {
     setTimeout(function() {
         block.text('');
-        $(currentForm).after(block);
+        block.fadeOut();
     }, 4000);
 }
 
-$('.btn-next').on('click', function() {
-    errorBlock.show();
-    let currentForm = $(this).data('form-id');
-    
-    if ( currentForm === 'app-form-step-1' ) {
-        currentForm = `#${currentForm}`;
-        const amountOfVariantsField = $(currentForm).find('.amount-of-variants');
-        const amountOfVariantsVal = $(amountOfVariantsField).val();
-        if ( amountOfVariantsVal.match(regExpObj.regExpNum) == null ) {
-            $(errorBlock).text(errors.notANumber);
-            $(amountOfVariantsField).after(errorBlock);
-            hideErrorBlock(currentForm, errorBlock);
+
+// node for custom events
+
+const eNode = $({});
+
+
+// gets nodes of steps & hides smth initially
+
+const stepOneWrapper = $('.step-1');
+
+const stepTwoWrapper = $('.step-2');
+stepTwoWrapper.hide();
+
+
+// step-1
+
+$('.btn-step-1').on('click', function() {
+    const currentForm = '#' + $(this).parent().attr('id');
+    const currentErrorBlock = $(currentForm).find('.error-block');
+    const amountOfVariantsField = $(currentForm).find('.amount-of-variants');
+    const amountOfVariantsVal = amountOfVariantsField.val();
+
+    currentErrorBlock.show();
+
+    if ( amountOfVariantsVal.length === 0 ) {
+        currentErrorBlock.text(errors.emptyField);
+        amountOfVariantsField.after(currentErrorBlock);
+        hideErrorBlock(currentForm, currentErrorBlock);
+    } else if ( amountOfVariantsVal.match(regExpObj.regExpSpaces) !== null ) {
+        currentErrorBlock.text(errors.usingSpaces);
+        amountOfVariantsField.after(currentErrorBlock);
+        hideErrorBlock(currentForm, currentErrorBlock);
+    } else if ( amountOfVariantsVal.match(regExpObj.regExpNum) == null ) {
+        currentErrorBlock.text(errors.notANumber);
+        amountOfVariantsField.after(currentErrorBlock);
+        hideErrorBlock(currentForm, currentErrorBlock);
+    } else {
+        if ( amountOfVariantsVal > 100 ) {
+            currentErrorBlock.text(errors.tooBigAmount);
+            amountOfVariantsField.after(currentErrorBlock);
+            hideErrorBlock(currentForm, currentErrorBlock);
+        } else {
+            $(currentForm).after(currentErrorBlock);
+
+            const variantsWrapperForm = $('#app-form-step-2');
+
+            stepTwoWrapper.prepend(`<p class="p">The amount of variants is: ${amountOfVariantsVal}</p>`);
+
+            for (let i = 1; i <= amountOfVariantsVal; i++) { // think about prefix / postfix notation of iterator
+                variantsWrapperForm.append(`
+                <div class="form-item">
+                    <label for="variant-${i}" class="std-form-label">Please, input the description of variant</label>
+                    <textarea id="variant-${i}" class="textarea"></textarea>
+                </div>`);
+            }
+
+            variantsWrapperForm.append('<button type="button" class="btn btn-primary btn-next btn-step-2" disabled>Launch random choice</button>');
+            variantsWrapperForm.append('<div class="error-block"></div>');
+            eNode.trigger('readyVariantsList');
         }
     }
+});
+
+eNode.on('readyVariantsList', function() {
+    stepOneWrapper.hide();
+    stepTwoWrapper.show();
 });
