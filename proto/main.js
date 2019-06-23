03ebab28de63d3df597d4cd0716ffea8a039cb64
Igor Ivanov <drkierkegor@gmail.com>
2019-06-23 23:49:06 +0300

    big logic updt: split to 2 scenarios

--- proto/main.js
+++ proto/main.js
@@ -2,6 +2,7 @@ const errors = {
     emptyField: 'Please, fill the field',
     notANumber: 'Please, use only numbers',
     usingSpaces: 'Please, don\'t use spaces',
+    tooSmallAmount: 'The amount should be more or equal to 2',
     tooBigAmount: 'The amount should be less or equal to 100',
 }
 
@@ -53,12 +54,6 @@ let textAreasCounter = 1;
 
 const stepOneCommonErrBlock = $('.step-1 .common-errors-block');
 
-$(document).ready(function() {
-    $('.std-form-label').first().attr('for', `variant-descr-${textAreasCounter}`);
-    $('.textarea').first().attr('id', `variant-descr-${textAreasCounter}`);
-});
-
-
 const btnLaunchRandom = $('.btn-launch-random');
 
 
@@ -66,7 +61,7 @@ const btnLaunchRandom = $('.btn-launch-random');
 
 $('.btn-add-variant').on('click', function() {
     $('#app-form-step-1 .form-item:last').after(`
-        <div class="form-item">
+        <div class="form-item scenario-1-item">
             <label for="variant-descr-${++textAreasCounter}" class="std-form-label">Please, input the description</label>
             <textarea id="variant-descr-${textAreasCounter}" class="textarea"></textarea>
             <a href="#" class="link link-del-variant">Delete variant</a>
@@ -81,12 +76,98 @@ $('.btn-add-variant').on('click', function() {
 });
 
 
+$('.btn-specify-amount').on('click', function() {
+    eNode.trigger('scenarioTwoActivated');
+});
+
+eNode.on('scenarioTwoActivated', function() {
+    if ( $('#app-form-step-1 .scenario-2-item').length > 0 ) {
+        textAreasCounter = $('#app-form-step-1 .scenario-2-item').length;
+        $('.scenario-2-item').fadeIn();
+        $('.btn-add-variants-auto').fadeOut();
+        btnLaunchRandom.fadeIn(); // test
+    } else {
+        textAreasCounter = 0;
+    }
+
+    $('.note-scenario-1').fadeOut();
+    $('.note-scenario-2').fadeIn();
+    $('.scenario-1-item').fadeOut();
+    $('.scenario-2').fadeIn();
+    $('.btn-add-variant').fadeOut();
+    btnLaunchRandom.fadeOut();
+    $('.btn-add-variants-auto').fadeIn().attr('disabled', true);
+});
+
+$('.btn-back-to-init').on('click', function() {
+    eNode.trigger('scenarioTwoDisactivated');
+});
+
+eNode.on('scenarioTwoDisactivated', function() {
+    $('.note-scenario-1').fadeIn();
+    $('.note-scenario-2').fadeOut();
+    $('.scenario-1-item').fadeIn();
+    $('.scenario-2-item').fadeOut();
+    $('.scenario-2').fadeOut();
+    $('.btn-add-variants-auto').fadeOut().attr('disabled', false);
+    $('.btn-add-variant').fadeIn();
+    if ( $('#app-form-step-1 .scenario-1-item').length >= 2 ) {
+        btnLaunchRandom.fadeIn().attr('disabled', false);
+    } else {
+        btnLaunchRandom.fadeIn().attr('disabled', true);
+    }
+});
+
+$('#amount-of-variants').on('input', function() {
+    $('.btn-add-variants-auto').attr('disabled', false);
+});
+
+$('#amount-of-variants').on('blur', function() {
+    if ( $(this).val().length === 0 ) {
+        $('.btn-add-variants-auto').attr('disabled', true);
+    }
+});
+
+$('.btn-add-variants-auto').on('click', function() {
+    const amountOfVariants = $('#amount-of-variants').val();
+
+    if ( amountOfVariants < 2 ) {
+        stepOneCommonErrBlock.text(errors.tooSmallAmount).fadeIn();
+        hideBlock(stepOneCommonErrBlock);
+    // replace to 'length > 100'
+    } else if ( amountOfVariants > 10 ) {
+        stepOneCommonErrBlock.text(errors.tooBigAmount).fadeIn();
+        hideBlock(stepOneCommonErrBlock);
+    } else {
+        // think about prefix / postfix notation of iterator
+        for (let i = 1; i <= amountOfVariants; i++) {
+            $('#app-form-step-1 .form-item:last').after(`
+                <div class="form-item scenario-2-item">
+                    <label for="variant-descr-${i}" class="std-form-label">Please, input the description</label>
+                    <textarea id="variant-descr-${i}" class="textarea"></textarea>
+                    <a href="#" class="link link-del-variant">Delete variant</a>
+                    <div class="error-block"></div>
+                </div>
+            `);
+
+            // test
+            ++textAreasCounter;
+            console.log(textAreasCounter);
+        }
+
+        $('.scenario-2-item:first').find('.link-del-variant').hide();
+        $(this).fadeOut();
+        btnLaunchRandom.fadeIn().attr('disabled', false);
+    }
+});
+
+
 // deleting variants
 
 $('#app-form-step-1').on('click', '.link-del-variant', function(e) {
     e.preventDefault();
 
-    if ( $('#app-form-step-1 .form-item').length !== 1 ) {
+    if ( $('#app-form-step-1 .scenario-1-item').length !== 1 || $('#app-form-step-1 .scenario-2-item').length !== 1 ) {
         --textAreasCounter; // check !
         $(this).parent().remove();
     }
@@ -94,6 +175,13 @@ $('#app-form-step-1').on('click', '.link-del-variant', function(e) {
     if (textAreasCounter < 2) {
         btnLaunchRandom.attr('disabled', true);
     }
+
+    if ( $('#app-form-step-1 .scenario-2-item').length === 1 ) {
+        $('#app-form-step-1 .scenario-2-item').remove();
+        $('.btn-add-variants-auto').fadeIn();
+    }
+
+    console.log(textAreasCounter); // test
 });
 
 
